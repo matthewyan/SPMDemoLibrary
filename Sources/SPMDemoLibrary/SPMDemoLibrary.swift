02de90a3b1292e867294d8df047bf51b04ab1355
matthewyan <pigphone@gmail.com>
2019-09-22 17:40:43 +0800

    update

--- Sources/SPMDemoLibrary/SPMDemoLibrary.swift
+++ Sources/SPMDemoLibrary/SPMDemoLibrary.swift
@@ -1,9 +1,7 @@
 public struct SPMDemoLibrary {
-    var text = "Hello, World!"
-}
+    public var text = "Hello, World!"
 
-public class MyTestCls {
-    public static func outputInfo() {
-        print("Hello, World!")
+    public func outputInfo() {
+        print(text)
     }
 }
